1fa4316b8c87d0c5d9183aaf2e0a089e9e4639cc
neil1117 <neil.jenche@gmail.com>
2017-07-22 15:32:49 +0800

    刪除多餘的部份

--- 222. Count Complete Tree Nodes/Count Complete Tree Nodes.c
+++ 222. Count Complete Tree Nodes/Count Complete Tree Nodes.c
@@ -1,64 +1,38 @@
 //
-//  main.c
+//  Count Complete Tree Nodes.c
 //  leetCode
 //
 //  Created by Neil Wu on 2017/7/13.
 //  Copyright © 2017年 Neil Wu. All rights reserved.
 //
 
-#include <stdio.h>
-
-struct TreeNode {
-    int val;
-    struct TreeNode *left;
-    struct TreeNode *right;
-};
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
 
 int countNodes(struct TreeNode* root) {
-    
-    int nodeCount = 0;
-    int leftLevel = 1;
-    int rightLevel = 1;
+    int level = 1;
     
     if(root == NULL)
         return 0;
     
-    struct TreeNode *temp = root;
-    while (temp -> right != NULL) {
-        temp = temp->right;
-        rightLevel += 1;
-    }
-    temp = root;
-    while (temp -> left != NULL) {
-        temp = temp->left;
-        leftLevel += 1;
-    }
-    if (rightLevel == leftLevel) {
-        nodeCount = pow(2, rightLevel)-1;
+    struct TreeNode *tempL = root;
+    struct TreeNode *tempR = root;
+    for (;tempL -> left != NULL; level++) {
+        tempL = tempL->left;
+        tempR = tempR->right;
     }
     
-    
-    return nodeCount;
+    if (tempR != NULL) {
+        //原本是用pow的方法去算2的次方，但是這個方法更快
+        return (1 << level) - 1;
+    }
+    else {
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
 }
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    struct TreeNode a;
-    struct TreeNode b;
-    struct TreeNode c;
-    
-    a.right = &b;
-    b.right = &c;
-    c.right = NULL;
-    int count = countNodes(&a);
-    printf("%d", count);
-    
-    return 0;
-}
-
-
-
-
-
-
